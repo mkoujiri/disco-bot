56174bbd27f0e8055bfdfb1d054d382a66dd1af6
Matthew Oujiri <mkoujiri@mtu.edu>
2024-05-02 19:57:27 -0400

    Did some magic

--- main.py
+++ main.py
@@ -3,27 +3,64 @@ import csv
 import os
 from random import *
 import requests
+import time
+import jsonpickle
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
-from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+APP_ID = "1142918946492784731"
+DISCO_SERVER_ID = '1011844778813558874'
 
 bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 
-client = discord.Client(intents=discord.Intents.all())
-meg_file='/home/vimguy/disco-bot/megs.csv'
-counts_file='/home/vimguy/disco-bot/counts.csv'
+LOCAL = True
+if not LOCAL:
+    meg_file='/home/vimguy/disco-bot/megs.csv'
+    counts_file='/home/vimguy/disco-bot/counts.csv'
+    apple_dir="/home/vimguy/disco-bot/assets/apples"
+    disco_dir="/home/vimguy/disco-bot/assets/discos"
+    summer_comp_dir='/home/vimguy/disco-bot/assets/summer'
+    summer_comp_file='/home/vimguy/disco-bot/assets/summer/log.csv'
+else:
+    meg_file='/home/mkoujiri/programming/disco_bot/megs.csv'
+    counts_file='/home/mkoujiri/programming/disco_bot/counts.csv'
+    apple_dir="/home/mkoujiri/programming/disco_bot/assets/apples"
+    disco_dir="/home/mkoujiri/programming/disco_bot/assets/discos"
+    summer_comp_dir='/home/mkoujiri/programming/disco_bot/assets/summer'
+    summer_comp_file='/home/mkoujiri/programming/disco_bot/assets/summer/log.csv'
 
 quotes_list = []
+
 
 def load_csv(file_name):
     with open(file_name, 'r') as csv_file:
         reader = csv.reader(csv_file)
         return dict(reader)
+
+class Activity:
+    def __init__(self,activity_type,points,description):
+        self.type = activity_type
+        self.points = points
+        self.description = description
+        self.time_stamp = time.time()
+
+class UserLog:
+    def __init__(self):
+        self.activities = []
+    def add_activity(self,activity_type,points,description):
+        self.activities.append(Activity(activity_type,points,description))
+    def total_points(self):
+        return sum([x.points for x in self.activities])
+    def display_activities(self):
+        output = "```\n"+"\n".join([f"{time.strftime('%Y-%m-%d', time.localtime(activity.time_stamp))}: {activity.type}, {activity.description}" for activity in sorted(self.activities,key=lambda activity: activity.time_stamp)])+"\n```"
+        return output
+
+
 
 def save():
     with open(meg_file, 'w') as csv_file:  
@@ -34,6 +71,12 @@ def save():
         counts_writer = csv.writer(csv_file)
         for key, value in counts.items():
            counts_writer.writerow([key, value])
+    with open(summer_comp_file, 'w') as f:
+        try:
+            f.write(jsonpickle.encode(summer_logs))
+        except Exception as e:
+            print(e)
+
 
 def read_quotes():
     # read all messages
@@ -48,15 +91,24 @@ def read_quotes():
     while True:
         data = requests.get(f"{API_ENDPOINT}/channels/{quotes_channel_id}/messages",headers=headers,params=inputs).json()
 
+        if(not isinstance(data,list)):
+            time.sleep(1)
+            continue
         for msg in data:
             # process the message
-            content = msg['content']
-            author = msg['author']['global_name']
-            time_stamp = msg['timestamp'][0:10]
-            if author is not None:
-                if "\"" in content or "-" in content:
-                    # message is a quote
-                    quotes_list.append(f"This was sent by {author} at {time_stamp}:\n {content}")
+            try:
+                content = msg['content']
+                author = msg['author']['global_name']
+                time_stamp = msg['timestamp'][0:10]
+                if author is not None:
+                    if "\"" in content or "-" in content:
+                        # message is a quote
+                        quotes_list.append(f"This was sent by {author} at {time_stamp}:\n {content}")
+            except Exception as e:
+                print(e)
+                print("failed to load quotes")
+                return
+        time.sleep(.5)
 
         # find oldest message
         inputs["before"]=data[-1]["id"]
@@ -64,6 +116,55 @@ def read_quotes():
             break
     print(f"loaded {len(quotes_list)} quotes")
 
+
+def save_attachments(ctx,directory):
+    try:
+        for attachment in ctx.message.attachments:
+            # image saved
+            ftype = attachment.url.split('/')[-1]
+            myfile = requests.get(attachment.url)
+            open(f'{directory}/{attachment.filename}', 'wb').write(myfile.content)
+    except AttributeError as e:
+        return
+
+@bot.command()
+async def adddisco(ctx):
+    save_attachments(ctx,disco_dir)
+
+@bot.command()
+async def addapple(ctx):
+    save_attachments(ctx,apple_dir)
+
+@bot.slash_command(name="summer",description="slash command")
+async def summer(ctx,activity_type: discord.Option(str,choices=["sprints","throws","film","lift","misc"]),points: discord.Option(int),description: discord.Option(str)):
+    save_attachments(ctx,summer_comp_dir)
+
+    user_id = f"{ctx.author.id}"
+    if user_id not in summer_logs:
+        summer_logs[user_id] = UserLog()
+    summer_logs[user_id].add_activity(activity_type,points,description)
+    save()
+    await ctx.respond(f"Added {activity_type}: {description}, you now have: {summer_logs[user_id].total_points()} points")
+
+@bot.command()
+async def summerlog(ctx, user: discord.Member = None):
+    user_id = f"{ctx.author.id}"
+    if(user):
+        user_id = f"{user.id}"
+    try:
+        await ctx.send(summer_logs[user_id].display_activities())
+    except KeyError as e:
+        pass
+
+@bot.command()
+async def summerboard(ctx):
+    output = "```\n"
+    for key,value in sorted(summer_logs.items(),key=lambda item: item[1].total_points(), reverse=True):
+        guild = await bot.fetch_guild(DISCO_SERVER_ID)
+        user = await guild.fetch_member(key)
+        output+=f"{user.nick},{value.total_points()}\n"
+    output+='```'
+    await ctx.send(output)
 
 @bot.command()
 async def meg(ctx, user: discord.Member = None):
@@ -97,6 +198,7 @@ async def randomquote(ctx):
 @bot.command()
 async def reload_quotes(ctx):
     read_quotes()
+    await ctx.send("Updated quotes");
 @bot.command()
 async def apple(ctx, count: int = 1):
     if 'apple' in counts:
@@ -133,20 +235,17 @@ async def reset(ctx):
 
 @bot.command()
 async def megboard(ctx):
-    print(megs.items())
     output = "```"+ "\n".join(f"{key}: {value}" for key,value in sorted(megs.items(),key=lambda item: item[1],reverse=True)) + "```"
     await ctx.send(output)
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-# megs = load_csv(meg_file)
-# counts = load_csv(counts_file)
+megs = load_csv(meg_file)
+counts = load_csv(counts_file)
+summer_logs = {}
+with open(summer_comp_file,"r") as f:
+    summer_logs = jsonpickle.decode(f.read())
+
+
 
 read_quotes()
-    
-
-
-
 
 bot.run(TOKEN)
